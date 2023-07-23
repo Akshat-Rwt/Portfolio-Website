cf9e697f3934ff938b7cf9c340695bb49e41943a
Akshat Rawat <111777625+Akshat-Rwt@users.noreply.github.com>
2023-07-23 11:58:02 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,8 @@
+console.log("Script running.....")
+<script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+    // <!-- Setup and start animation! -->
+  <script>
+    var typed = new Typed('#element', {
+      strings: ['Web Developer','Graphic Designer' , 'Web Designer','Video Edictor' ],
+      typeSpeed: 50,
+    });
